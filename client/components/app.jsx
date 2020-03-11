d9d8949d8ceb6bbfd5c9b70ddf9fceebcc0fff7a
Arlette Pineda <arlettepimo@gmail.com>
2020-03-11 20:04:07 +0000

    Created new file product-details, added product prop to state and tested on app.jsx.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Header from './header';
-import ProductList from './product-list';
+// import Header from './header';
+// import ProductList from './product-list';
+import ProductDetails from './product-details';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,8 +15,9 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />,
-        <ProductList />
+        {/* <Header />,
+        <ProductList /> */}
+        <ProductDetails />
       </div>
     );
   }
