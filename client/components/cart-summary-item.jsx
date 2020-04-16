4d64af93286347fcc169a1f0baf070d07abbc5af
Arlette Pineda <arlettepimo@gmail.com>
2020-04-16 20:39:19 +0000

    Created new CartSummaryItem component with html to create card.

---
+++ client/components/cart-summary-item.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function CartSummaryItem(props) {
+  return (
+    <div className="card">
+      <div className="row">
+        <div className="">
+          <img src="" alt=""/>
+          Card Image Here
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h3 className="card-title">Item Name</h3>
+            <p className="card-text">Price Here</p>
+            <p className="card-text">Short Description Here. Short Description Here. Short Description Here. Short Description Here.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
