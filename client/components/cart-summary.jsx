60a6f59654856e8e2d8af8f78a6816bcbcc5cecf
Arlette Pineda <arlettepimo@gmail.com>
2020-04-16 23:44:39 +0000

    Added conditional statement to let user know if no items in cart.

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
 function CartSummary(props) {
+
+  if (props.cart.length === 0) {
+    return (
+      <div>
+        <p>You haven&#39;t added anything yet!</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
       <h2>My Cart</h2>
@@ -19,7 +28,7 @@ function CartSummary(props) {
         }
         )
       }
-      <h3>Item Total $</h3>
+      <h3>Item Total $ x</h3>
     </div>
   );
 }
