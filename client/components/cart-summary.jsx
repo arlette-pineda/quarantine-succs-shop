5b0e4845513c23720ef263e6fc1fc29c6ae91a12
Arlette Pineda <arlettepimo@gmail.com>
2020-04-16 23:33:42 +0000

    Mapped cart array and passed props to CartSummaryItem.

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import CartSummaryItem from './cart-summary-item';
 
 function CartSummary(props) {
   return (
-
-  )
+    <div className="container">
+      <h2>My Cart</h2>
+      {props.cart.map(cartItem =>
+        return (
+          <CartSummaryItem
+            key={cartItem.cartItemId}
+            image={cartItem.image}
+            itemName={cartItem.name}
+            price={cartItem.price}
+            shortDesc={cartItem.shortDescription}
+          />
+        ))}
+    </div>
+  );
 }
