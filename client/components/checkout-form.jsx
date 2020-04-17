d37f0751ebaf2c702a9a55cd9882b617a78eba9a
Arlette Pineda <arlettepimo@gmail.com>
2020-04-17 05:59:28 +0000

    Added pointer cursor to continue shopping element.

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -85,7 +85,7 @@ class CheckoutForm extends React.Component {
             </textarea>
           </div>
           <div className="row mt-5 ml-2 mr-2 d-flex justify-content-between">
-            <small className="text-muted"> &lt; Continue Shopping</small>
+            <small className="text-muted pointer"> &lt; Continue Shopping</small>
             <button type="submit" className="btn btn-primary">Place Order</button>
           </div>
         </form>
