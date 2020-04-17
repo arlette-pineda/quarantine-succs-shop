70d55210da8385c11b5fbc64bc364ad09eca212a
Arlette Pineda <arlettepimo@gmail.com>
2020-04-17 06:37:09 +0000

    Modified Header to stateless component, function.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function Header(props) {
+  const cartItemCount = props.cartItemCount.length;
+  const setView = props.setView;
 
-  render() {
-    const cartItemCount = this.props.cartItemCount.length;
-    const setView = this.props.setView;
-    return (
-      <div className="container">
-        <div className="header-style row p-2 ">
-          <div className="col-9">
-            <p>$ Wicked Sales</p>
-          </div>
-          <div className="col d-flex alighn-items-center justify-content-center pointer"
-            onClick={() => setView('cart', {})}>
-            <p className="m-2">{`${cartItemCount} items`}</p>
-            <i className="fas fa-shopping-cart fa-2x"></i>
-          </div>
+  return (
+    <div className="container">
+      <div className="header-style row p-2 ">
+        <div className="col-9">
+          <p>$ Wicked Sales</p>
+        </div>
+        <div className="col d-flex alighn-items-center justify-content-center pointer"
+          onClick={() => setView('cart', {})}>
+          <p className="m-2">{`${cartItemCount} items`}</p>
+          <i className="fas fa-shopping-cart fa-2x"></i>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Header;
