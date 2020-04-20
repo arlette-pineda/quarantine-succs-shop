1339c455edba1556a71a2442e9ada66d90c9bf87
Arlette Pineda <arlettepimo@gmail.com>
2020-04-20 10:50:32 +0000

    Deleted unused classes on product details.

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -27,12 +27,12 @@ class ProductDetails extends React.Component {
       return (
         <div className="container prod-details-style col-md-7">
           <small className="row text-muted ml-2 mb-2 mt-4 pointer" onClick={() => this.props.setView('catalog', {})}> &lt;Back to catalog </small>
-          <div className="row" id="for-body-div">
+          <div className="row">
             <img className="col-5 d-inline img-style img-fluid" src={this.state.product.image} />
-            <div className="col-7" id="div-for-name-shortdes-price">
-              <h2 className="">{this.state.product.name}</h2>
+            <div className="col-7">
+              <h2>{this.state.product.name}</h2>
               <div className=" card-subtitle text-muted">${(this.state.product.price / 100).toFixed(2)}</div>
-              <div className="">
+              <div>
                 {this.state.product.shortDescription}
               </div>
               <button onClick={() => this.props.addToCart(this.state.product)}
