0cec59a487c4a4c106c45a76d6e3925a818f253a
Arlette Pineda <arlettepimo@gmail.com>
2020-04-20 11:01:05 +0000

    Escaped character.

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -29,10 +29,10 @@ class ProductDetails extends React.Component {
           <small className="row text-muted ml-2 mb-2 mt-4 pointer" onClick={() => this.props.setView('catalog', {})}> &lt;Back to catalog </small>
           <div className="row">
             <img className="col-5 d-inline img-style img-fluid" src={this.state.product.image} />
-            <div className="col-7">
+            <div className="col-7 card-body">
               <h2>{this.state.product.name}</h2>
-              <div className=" card-subtitle text-muted">${(this.state.product.price / 100).toFixed(2)}</div>
-              <div>
+              <div className="card-subtitle text-muted">${(this.state.product.price / 100).toFixed(2)}</div>
+              <div className="card-text">
                 {this.state.product.shortDescription}
               </div>
               <button onClick={() => this.props.addToCart(this.state.product)}
