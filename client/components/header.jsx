f5634b1dbb8b670d9e34c295b80ed51d2b1ef85f
Arlette Pineda <arlettepimo@gmail.com>
2020-04-21 22:11:57 +0000

    Added margin to bottom of header.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,7 +5,7 @@ function Header(props) {
   const setView = props.setView;
 
   return (
-    <div className="container-lg p-2 px-4 d-flex header-style">
+    <div className="container-lg p-2 px-4 d-flex header-style mb-4">
       <div className="d-inline">
         <h2 className="hover"
           onClick={() => props.setView('catalog', {})}>
