fe8fced5c3650bd6170c951f4a9cd54c2912f144
Arlette Pineda <arlettepimo@gmail.com>
2020-05-13 22:04:59 +0000

    Changed product item font.

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -6,7 +6,7 @@ function ProductListItem(props) {
       <div className="card border-0 h-100 hover" key={props.product.productId}>
         <img src={props.product.image} className="card-img-top img-style h-50" />
         <div className="card-body">
-          <h5 className="card-title">{props.product.name}</h5>
+          <h5 className="card-title font-style">{props.product.name}</h5>
           <h6 className="card-subtitle mb-2 text-muted"> ${(props.product.price / 100).toFixed(2)}</h6>
           <p className="card-text">{props.product.shortDescription}</p>
         </div>
