caaf896261c067b4d8acdbf6da9ba3e253b118f0
Arlette Pineda <arlettepimo@gmail.com>
2020-05-14 23:57:26 +0000

    Added object fit to icon.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -7,7 +7,7 @@ function Header(props) {
   return (
     <div className="container-lg px-4 p-5 d-flex header-style pt-5 mb-5 ">
       <div className="d-inline" onClick={() => props.setView('catalog', {})}>
-        <img className="d-inline icon-size glow pr-1 pb-2" src="/images/echeveria.png" alt="echeveria"/>
+        <img className="d-inline icon-size glow pb-2" src="/images/echeveria.png" alt="echeveria"/>
         <h2 className="d-inline glow shrink brand-font"
         >
              Quarantine Succs</h2>
