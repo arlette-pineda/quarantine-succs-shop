e7444eb4a1ec53f32dcf60e7ed45f4875b781d6f
Arlette Pineda <arlettepimo@gmail.com>
2020-05-19 20:04:21 +0000

    Added carousel class and added state, images.

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -1,3 +1,20 @@
 import React from 'react';
 
+class Carousel extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentImageIndex: 0,
+      images: [
+        '/images/carousel1.jpg',
+        '/images/carousel2.jpg',
+        '/images/carousel3.jpg',
+        '/images/carousel4.jpg',
+        '/images/carousel5.jpg',
+        '/images/carousel6.jpg'
+      ]
+    };
+  }
+}
+
 export default Carousel;
