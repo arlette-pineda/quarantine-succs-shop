74bf98a29d82e4c0b2c814952d0aa9cc03a17408
Arlette Pineda <arlettepimo@gmail.com>
2020-05-20 23:32:20 +0000

    Passed Carousel to app in catalog view.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -88,6 +88,7 @@ export default class App extends React.Component {
       return (
         <div>
           <Header cartItemCount={this.state.cart} setView={this.setView} />
+          <Carousel />
           <ProductList setView={this.setView} view={this.state.view} />
         </div>
       );
