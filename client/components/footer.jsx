17a56f2dd64775100c45eb9c3ff20435f7f359be
Arlette Pineda <arlettepimo@gmail.com>
2020-05-26 23:27:16 +0000

    Defined sub footer className.

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className="py-5 header-style">
       <div className="container">
         <h3 className="text-center brand-font">Quarantine Succs</h3>
-        <h6 className="text-center sub-header">&quot;The perfect quiet companion.&quot;</h6>
+        <h6 className="text-center sub-footer">&quot;The perfect quiet companion.&quot;</h6>
       </div>
     </div>
   );
