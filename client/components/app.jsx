6ac5950b00cbb61ac11f695766d573424bbebfee
Arlette Pineda <arlettepimo@gmail.com>
2020-05-26 23:19:05 +0000

    Imported footer to app.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header';
 import Carousel from './carousel';
+import Footer from './footer';
 import ProductList from './product-list';
 import ProductDetails from './product-details';
 import CartSummary from './cart-summary';
