f6d1f249bb65ac48c585c6208aac93666edb406e
Arlette Pineda <arlettepimo@gmail.com>
2020-05-26 23:17:42 +0000

    Created footer component.

---
+++ client/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="py-5 header-style">
+      <div className="container">
+        <h3 className="text-center brand-font">Quarantine Succs</h3>
+        <h6 className="text-center sub-header">&quot;The perfect quiet companion.&quot;</h6>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
