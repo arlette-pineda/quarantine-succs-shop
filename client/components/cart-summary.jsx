122423ad68a072b57259cb42dbf642d7d94e0d37
Arlette Pineda <arlettepimo@gmail.com>
2020-05-27 01:28:19 +0000

    Adjusted cart summary margin.

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -10,12 +10,12 @@ function CartSummary(props) {
 
   if (props.cart.length === 0) {
     return (
-      <div className="mx-3 py-5 cart-height">
+      <div className="mx-4 py-5 cart-height">
         <small className="row back ml-2 mb-2 mt-2 pointer"
           onClick={() => props.setView('catalog', {})}>
           &lt;Back to catalog
         </small>
-        <p className="text-center pt-5">You haven&#39;t added anything yet!</p>
+        <p className="text-center pt-5 empty">You haven&#39;t added anything yet!</p>
       </div>
     );
   }
