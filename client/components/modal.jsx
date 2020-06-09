fb9063b5bd04c3d18e1ac1d994b1fbdc1400cc3a
Arlette Pineda <arlettepimo@gmail.com>
2020-06-09 22:23:40 +0000

    Added dislaimer info.

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 function Disclaimer(props) {
   return (
-    <div className="vh-100">
-      <div>Hi</div>
+    <div className="d-flex vh-100 align-items-center modal">
+      <div className="text-center justify-content-center modal-box">
+        Quarantine Succs is a full-stack e-commerce application built for demonstration purposes only.
+        <p>By clicking, "I accept" you are acknowledging that personal information shall not be used,
+          and that real purchases cannot be made.</p>
+      </div>
     </div>
   );
 }
