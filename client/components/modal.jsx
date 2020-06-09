07d2c5433492ca4edaf67193d2446dc7a20532d6
Arlette Pineda <arlettepimo@gmail.com>
2020-06-09 22:25:03 +0000

    Added I accept button and escaped quotes.

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -5,8 +5,9 @@ function Disclaimer(props) {
     <div className="d-flex vh-100 align-items-center modal">
       <div className="text-center justify-content-center modal-box">
         Quarantine Succs is a full-stack e-commerce application built for demonstration purposes only.
-        <p>By clicking, "I accept" you are acknowledging that personal information shall not be used,
+        <p>By clicking, &quot;I accept&quot; you are acknowledging that personal information shall not be used,
           and that real purchases cannot be made.</p>
+        <button type="button" className="btn btn-success">I accept</button>
       </div>
     </div>
   );
