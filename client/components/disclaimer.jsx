6484a0c1016e71833e9b610050b6dfc1897b697e
Arlette Pineda <arlettepimo@gmail.com>
2020-06-09 23:28:54 +0000

    Redid disclaimer modal, added to app.

--- client/components/disclaimer.jsx
+++ client/components/disclaimer.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function Disclaimer(props) {
   return (
     <div className="modal">
-      <div className="text-center modal-box p-4">
+      <div className="text-center modal-box p-5">
         <h1><span className="font-style">Quarantine Succs</span><br></br> is a full-stack e-commerce application built for demonstration purposes only.</h1>
-        <p className="modal-p">By clicking, &quot;I accept&quot; you are acknowledging that personal information shall not be used,
+        <br></br><p className="modal-p">By clicking, &quot;I accept&quot; you are acknowledging that personal information shall not be used,
           and that real purchases cannot be made.</p>
-        <button onClick={() => props.setView('catalog', {})}
+        <button onClick={() => props.handleDisclaimer()}
           type="button" className="btn btn-success">I accept</button>
       </div>
     </div>
