6871d304ec44b6e49ec27a50f9a57d95e4fba876
Arlette Pineda <arlettepimo@gmail.com>
2020-06-12 23:55:03 +0000

    Removed backdrop.

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -43,6 +43,11 @@ class ProductDetails extends React.Component {
     if (this.state.product != null) {
       return (
         <div>
+          {this.state.modalShowing ? <div onClick={() => this.closeModal()} className="modal-drop"></div> : null }
+          <AddModal modalShowing={this.state.modalShowing}
+            showModal={this.showModal}
+            closeModal={this.closeModal}
+            setView={this.props.setView} />
           <div className="container col-md-9 mb-2 cart-height">
             <small className="row ml-2 my-5 mb-3 back" onClick={() => this.props.setView('catalog', {})}> &lt;Back to catalog </small>
             <div className="row">
@@ -64,10 +69,7 @@ class ProductDetails extends React.Component {
               {this.state.product.longDescription}
             </div>
           </div>
-          <AddModal modalShowing={this.state.modalShowing}
-            showModal={this.showModal}
-            closeModal={this.closeModal}
-            setView={this.props.setView} />
+
         </div>
       );
     } else {
