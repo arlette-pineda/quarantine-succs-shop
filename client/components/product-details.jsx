ccc28101d9751e27d8b6494652fbfaf8d549d56f
Arlette Pineda <arlettepimo@gmail.com>
2020-06-15 00:03:54 +0000

    Margin edit.

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -42,7 +42,7 @@ class ProductDetails extends React.Component {
   render() {
     if (this.state.product != null) {
       return (
-        <div className="mt-4">
+        <div className="mt-5">
           <div className="container col-md-9 mb-2 mt-5 cart-height">
             <small className="row ml-2 my-5 mb-3 back" onClick={() => this.props.setView('catalog', {})}> &lt;Back to catalog </small>
             <div className="row">
