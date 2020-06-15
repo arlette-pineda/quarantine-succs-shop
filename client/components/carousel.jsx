99d01187268c62b488bd535c268de05af128ca67
Arlette Pineda <arlettepimo@gmail.com>
2020-06-15 23:44:15 +0000

    Added h3 and caption to first image; deleted extra images.

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -19,25 +19,36 @@ class Carousel extends React.Component {
           // adaptiveHeight={true}
           className="slider"
         >
-          <div className='d-flex justify-content-center align-items-center'>
+          <div className='d-flex text-white'>
             <img className='carousel-img' src="/images/carousel0.jpg" alt="0" />
-            <div className="slide__caption">&quot;Caption 0 here. Caption 0 here.Caption 0 here. Caption 0 here.&quot;</div>
+            <div className="slide__caption">
+              <h3>Succulent Companionship</h3>
+              <p>&quot;Succulents, with their striking forms, unusual colors, and easy-care reputation, are fantastic indoor plants ready to keep you company wherever you may be.&quot;</p>
+            </div>
           </div>
-          <div className='d-flex justify-content-center align-items-center'>
+          <div className='d-flex '>
             <img className='carousel-img' src="/images/carousel1.jpg" alt="1"/>
-            <div className="slide__caption">&quot;Caption 1 here. Caption 1 here.Caption 1 here. Caption 1 here.&quot;</div>
+            <div className="slide__caption">
+              &quot;Caption 1 here. Caption 1 here.Caption 1 here. Caption 1 here.&quot;
+            </div>
           </div>
-          <div className='d-flex justify-content-center align-items-center'>
+          <div className='d-flex '>
             <img className='carousel-img' src="/images/carousel2.jpg" alt="2" />
-            <div className="slide__caption">&quot;Caption 2 here. Caption 2 here.Caption 2 here. Caption 2 here.&quot;</div>
+            <div className="slide__caption">
+              &quot;Caption 2 here. Caption 2 here.Caption 2 here. Caption 2 here.&quot;
+            </div>
           </div>
-          <div className='d-flex justify-content-center align-items-center'>
+          <div className='d-flex '>
             <img className='carousel-img' src="/images/carousel3.jpg" alt="3" />
-            <div className="slide__caption">&quot;Caption 3 here. Caption 3 here.Caption 3 here. Caption 3 here.&quot;</div>
+            <div className="slide__caption">
+              &quot;Caption 3 here. Caption 3 here.Caption 3 here. Caption 3 here.&quot;
+            </div>
           </div>
           <div>
             <img className='carousel-img' src="/images/carousel4.jpg" alt="4" />
-            <div className="slide__caption">&quot;Caption 4 here. Caption 4 here.Caption 4 here. Caption 4 here.&quot;</div>
+            <div className="slide__caption">
+              &quot;Caption 4 here. Caption 4 here.Caption 4 here. Caption 4 here.&quot;
+            </div>
           </div>
         </Slider>
       </div>
