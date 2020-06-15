31461f4fa71313673cab676936e5a3dfd2895ec7
Arlette Pineda <arlettepimo@gmail.com>
2020-06-15 00:01:00 +0000

    Deleted unused code.

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -63,7 +63,6 @@ class ProductDetails extends React.Component {
               {this.state.product.longDescription}
             </div>
           </div>
-          {this.state.modalShowing ? <div onClick={() => this.closeModal()} ></div> : null}
           <AddModal modalShowing={this.state.modalShowing}
             showModal={this.showModal}
             closeModal={this.closeModal}
