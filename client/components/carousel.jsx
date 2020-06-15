6debe2433df2bf7bab100de9f0189428609dc650
Arlette Pineda <arlettepimo@gmail.com>
2020-06-15 23:58:26 +0000

    Added all captions and h3's.

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -29,25 +29,33 @@ class Carousel extends React.Component {
           <div className='d-flex '>
             <img className='carousel-img' src="/images/carousel1.jpg" alt="1"/>
             <div className="slide__caption">
-              &quot;Caption 1 here. Caption 1 here.Caption 1 here. Caption 1 here.&quot;
+              <h3>Menthal Health</h3>
+              <p>&quot;Studies have shown that interaction with indoor plants may reduce psychological and physiological stress – in fact they can have a soothing effect.&quot;
+              </p>
             </div>
           </div>
-          <div className='d-flex '>
-            <img className='carousel-img' src="/images/carousel2.jpg" alt="2" />
+          <div className='d-flex text-white'>
+            <img className='carousel-img' src="/images/carousel20.jpg" alt="20" />
             <div className="slide__caption">
-              &quot;Caption 2 here. Caption 2 here.Caption 2 here. Caption 2 here.&quot;
+              <h3>Add Some Green</h3>
+              <p>&quot;Add a pop of natural green to liven up any room.&quot;
+              </p>
             </div>
           </div>
           <div className='d-flex '>
             <img className='carousel-img' src="/images/carousel3.jpg" alt="3" />
             <div className="slide__caption">
-              &quot;Caption 3 here. Caption 3 here.Caption 3 here. Caption 3 here.&quot;
+              <h3>Let Them Be Your Cheerleaders</h3>
+              <p>&quot;Whether painting, reading, or gaming let the little ones quietly cheer you on.&quot;
+              </p>
             </div>
           </div>
           <div>
             <img className='carousel-img' src="/images/carousel4.jpg" alt="4" />
             <div className="slide__caption">
-              &quot;Caption 4 here. Caption 4 here.Caption 4 here. Caption 4 here.&quot;
+              <h3>Quarantine Succs</h3>
+              <p>&quot;Let it suck a little less.&quot;
+              </p>
             </div>
           </div>
         </Slider>
