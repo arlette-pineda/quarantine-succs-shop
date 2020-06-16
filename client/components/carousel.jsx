2e4fc935e0cdaa05d68953818121c562342a4d1e
Arlette Pineda <arlettepimo@gmail.com>
2020-06-16 21:34:45 +0000

    Renamed carousel photos.

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -19,43 +19,39 @@ class Carousel extends React.Component {
           // adaptiveHeight={true}
           className="slider"
         >
-          <div className='d-flex text-white'>
-            <img className='carousel-img' src="/images/carousel0.jpg" alt="0" />
+          <div className='d-flex text-white justify-content-center'>
+            <img className='carousel-img' src="/images/carousel1.jpg" alt="1" />
             <div className="slide__caption">
               <h3>Succulent Companionship</h3>
               <p>&quot;Succulents, with their striking forms, unusual colors, and easy-care reputation, are fantastic indoor plants ready to keep you company wherever you may be.&quot;</p>
             </div>
           </div>
-          <div className='d-flex '>
-            <img className='carousel-img' src="/images/carousel1.jpg" alt="1"/>
+          <div className='d-flex justify-content-center'>
+            <img className='carousel-img ' src="/images/carousel2.jpg" alt="2"/>
             <div className="slide__caption">
               <h3>Menthal Health</h3>
-              <p>&quot;Studies have shown that interaction with indoor plants may reduce psychological and physiological stress – in fact they can have a soothing effect.&quot;
-              </p>
+              <p>&quot;Studies have shown that interaction with indoor plants may reduce psychological and physiological stress – in fact they can have a soothing effect.&quot;</p>
             </div>
           </div>
-          <div className='d-flex text-white'>
-            <img className='carousel-img' src="/images/carousel20.jpg" alt="20" />
-            <div className="slide__caption">
-              <h3>Add Some Green</h3>
-              <p>&quot;Add a pop of natural green to liven up any room.&quot;
-              </p>
-            </div>
-          </div>
-          <div className='d-flex '>
+          <div className='d-flex text-white justify-content-center'>
             <img className='carousel-img' src="/images/carousel3.jpg" alt="3" />
             <div className="slide__caption">
-              <h3>Let Them Be Your Cheerleaders</h3>
-              <p>&quot;Whether painting, reading, or gaming let the little ones quietly cheer you on.&quot;
-              </p>
+              <h3>Add Some Green</h3>
+              <p>&quot;Add a pop of natural green to liven up any room.&quot;</p>
             </div>
           </div>
-          <div>
+          <div className='d-flex justify-content-center'>
             <img className='carousel-img' src="/images/carousel4.jpg" alt="4" />
             <div className="slide__caption">
+              <h3>Let Them Be Your Cheerleaders</h3>
+              <p>&quot;Whether painting, reading, or gaming let the little ones quietly cheer you on.&quot;</p>
+            </div>
+          </div>
+          <div className='d-flex justify-content-center'>
+            <img className='carousel-img' src="/images/carousel5.jpg" alt="5" />
+            <div className="slide__caption">
               <h3>Quarantine Succs</h3>
-              <p>&quot;Let it suck a little less.&quot;
-              </p>
+              <p>&quot;Let it suck a little less.&quot;</p>
             </div>
           </div>
         </Slider>
