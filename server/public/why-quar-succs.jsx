8ff893d4a3902e69faa4ae0b921b106cac9394d8
Arlette Pineda <arlettepimo@gmail.com>
2020-06-26 22:08:37 +0000

    Created component for why QS was created.

---
+++ server/public/why-quar-succs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Why(props) {
+  return (
+    <div className="why">
+      <h2 >Why Quarantine Succs?</h2>
+      <div className="divider"></div>
+      <div>
+        <p>Because</p>
+      </div>
+    </div>
+  );
+}
+
+export default Why;
