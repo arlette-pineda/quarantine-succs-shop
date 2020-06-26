d4c7aeea25f48f373c4ccb1e45da1dc63c1b2962
Arlette Pineda <arlettepimo@gmail.com>
2020-06-26 23:51:21 +0000

    Added two p elements to Why component.

--- client/components/why-quar-succs.jsx
+++ client/components/why-quar-succs.jsx
@@ -6,7 +6,14 @@ function Why(props) {
       <h2 >Why Quarantine Succs?</h2>
       <div className="divider"></div>
       <div>
-        <p>Because</p>
+        <p>2020 has been a year full of surprises and hurdles. From abruptly transitioning to working from home,
+          playing toilet paper scavenger hunt, and everyone having to learn that the mask needs to cover your nose as well,
+          its been tough. We hope you still like the people you live with...
+        </p>
+        <p>Relieve some of that stress with your very own green companion that will always listen but never get you stuck
+          in a meeting that could&#39;ve been an email, judge you for still being in pajamas, or make fun of you for the quarantine
+          haircut you decided on giving yourself.
+        </p>
       </div>
     </div>
   );
